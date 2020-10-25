1b617de597961ef55ad2bc329c88c2921f195c0c
千葉陸矢 <chibarikuya@chibarikuyanoMacBook-Air.local>
2020-10-25 19:02:18 +0900

    Fix new_article_view

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,16 @@
 class FavoritesController < ApplicationController
 
   def create
-    @favorite = current_user.favorites.new(article_id: params[:article_id])
-    @favorite.save
-    redirect_to articles_path
+    favorite = current_user.favorites.new(article_id: params[:article_id])
+    favorite.save
+    @article = Article.find(params[:article_id])
+    redirect_to article_path(@article.id)
   end
 
   def destroy
-    @favorite = Favorite.find_by(user_id: current_user.id, article_id: params[:article_id])
-    @favorite.destroy
-    redirect_to articles_path
+    favorite = Favorite.find_by(user_id: current_user.id, article_id: params[:article_id])
+    favorite.destroy
+    @article = Article.find(params[:article_id])
+    redirect_to article_path(@article.id)
   end
 end
